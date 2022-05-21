fb459711543838e8f1626528c89ef14189e89f7a
sjkato <sjkato@gmail.com>
2022-05-21 14:55:13 +0100

    Update: wired up MunroListFragment and MunroListViewModel

--- app/src/main/java/com/katocoding/munrolibrarychallenge/ui/munrolist/MunroListViewState.kt
+++ app/src/main/java/com/katocoding/munrolibrarychallenge/ui/munrolist/MunroListViewState.kt
@@ -1,10 +1,14 @@
 package com.katocoding.munrolibrarychallenge.ui.munrolist
 
+import com.katocoding.munrolibrarychallenge.DEFAULT_DOUBLE
+import com.katocoding.munrolibrarychallenge.DEFAULT_STRING
+import com.katocoding.munrolibrarychallenge.data.munrolist.filter.HillCategoryType
+
 data class MunroListViewState(
-    val name: String,
-    val heightM: String,
-    val hillCategory: String,
-    val gridReference: String,
+    val name: String = DEFAULT_STRING,
+    val heightM: Double = DEFAULT_DOUBLE,
+    val hillCategory: HillCategoryType = HillCategoryType.Munro,
+    val gridReference: String = DEFAULT_STRING,
     val showLoading: Boolean = false,
     val showError: Boolean = false,
     val errorMessage: String = ""
